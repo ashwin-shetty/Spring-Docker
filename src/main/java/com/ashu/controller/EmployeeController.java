a62b4f1679c3be32fdf834008c6678a1bf3ad58f
ashwin <ashwin.shetty@hotmail.com>
2018-12-04 23:16:39 +0530

    Employee - Spingboot + Docker Image

--- src/main/java/com/ashu/controller/EmployeeController.java
+++ src/main/java/com/ashu/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private static final String template = "Hello, %s!";
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/employee")
